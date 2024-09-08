0ca80e4d620ffae714248eb11bd614a17a99bcbe
Adhicitta Masran <adhicittamasran@gmail.com>
2024-09-08 15:44:09 +0800

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Social from "./Social";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Social part={"footer-social"}/>
+
+      <p>Copyright © 2024 Adhicitta Masran. All Right Reserved.</p>
+    </footer>
+  )
+}
