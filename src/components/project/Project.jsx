6b96563e5c99cb5f69c4ca08b1134b904f76c3f4
Adhicitta Masran <adhicittamasran@gmail.com>
2024-10-26 23:07:12 +0800

    add filter projects by tag

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { getAllProject } from "../../data"
 import ProjectList from "./ProjectList"
 
 export default function Project() {
+  const tags = ['all', 'backend', 'frontend']
+  const [selectedTag, setSelectedTag] = useState('all')
+
   const projects = getAllProject()
 
   return (
     <section id="projects" className="projects">
       <h1 className="heading-1">Projects</h1>
-      <ProjectList data={projects}/>
+      <div className="project-filter">
+        {tags.map( v => (
+          <label className="tag-btn" key={v}>
+            <input type="radio" name="project-filter" value={v} checked={selectedTag === v} onChange={(e) => setSelectedTag(e.target.value)}/>
+            {v}
+          </label>
+        ))}
+      </div>
+
+      <ProjectList data={
+        selectedTag === 'all' ? projects : projects.filter(v => v.tags.includes(selectedTag))
+      }/>
     </section>
   )
 }
--- src/components/project/ProjectItem.jsx
+++ src/components/project/ProjectItem.jsx
@@ -2,9 +2,19 @@ import PropTypes from 'prop-types';
 import sourceCodeLogo from '../../assets/images/source-code.svg';
 import externalLinkLogo from '../../assets/images/external-link.svg';
 
-export default function ProjectItem({title, description, tech, code, src}) {
+export default function ProjectItem({title, description, tech, tags, code, src}) {
+  let dataCategory = ''
+  const tagsLength = tags.length
+  tags.forEach((v, i) => {
+    if (i != tagsLength-1) {
+      dataCategory += v + ' '
+    } else {
+      dataCategory += v
+    }
+  })
+
   return (
-    <li className='card project-item'>
+    <li className='card project-item' data-category={dataCategory}>
       <h2>{title}</h2>
       <p className="project-item-desc">{description}</p>
       <ul className="project-tech">
@@ -40,6 +50,7 @@ ProjectItem.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   tech: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
   code: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
 }
